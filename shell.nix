0b784bfae17deba65ce9c199f21ec87048f1788d
Niels G. W. Serup <ngws@metanohi.name>
2020-11-02 20:09:32 +0100

    Simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
-# You can run nix-shell in this directory if you have Nix installed.
-
+# Use this file with nix-shell or similar tools; see https://nixos.org/
 with import <nixpkgs> {};
-stdenv.mkDerivation {
-    name = "futcam";
-    buildInputs = [ pkgconfig SDL2 SDL2_ttf ocl-icd opencl-headers (python3.withPackages (ps: with ps; [ setuptools numpy pygame pyopencl opencv4 ])) ];
+
+mkShell {
+  buildInputs = [
+    pkgconfig
+    ocl-icd
+    opencl-headers
+    SDL2
+    SDL2_ttf
+    (python3.withPackages (ppkgs: with ppkgs; [ setuptools numpy pygame pyopencl opencv4 ]))
+  ];
 }
